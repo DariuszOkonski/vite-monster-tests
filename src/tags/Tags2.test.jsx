f6a319a114dbbf1798b936aaa869f7c000447747
Dariusz Okoński <darek200180@gmail.com>
2025-05-29 14:41:05 +0200

    add tests for tags

--- src/tags/Tags2.test.jsx
+++ src/tags/Tags2.test.jsx
@@ -1,8 +1,17 @@
 import { render, screen } from '@testing-library/react';
-import { afterAll, afterEach, beforeAll, describe, expect, it } from 'vitest';
+import {
+  afterAll,
+  afterEach,
+  beforeAll,
+  describe,
+  expect,
+  it,
+  vi,
+} from 'vitest';
 import Tags from './Tags';
 import { setupServer } from 'msw/node';
 import { http, HttpResponse } from 'msw';
+import axios from 'axios';
 
 describe('Tags', () => {
   const server = setupServer(
@@ -16,6 +25,22 @@ describe('Tags', () => {
   afterEach(() => server.resetHandlers());
 
   it('renders tags', async () => {
+    render(<Tags />);
+
+    const tags = await screen.findAllByTestId('tag');
+
+    expect(tags).toHaveLength(1);
+    expect(tags[0]).toHaveTextContent('bar');
+  });
+});
+
+describe('Tags not recommended', () => {
+  it('renders tags - not recommended', async () => {
+    const mockREsponse = {
+      data: [{ id: '1', name: 'bar' }],
+    };
+    vi.spyOn(axios, 'get').mockResolvedValue(mockREsponse);
+
     render(<Tags />);
 
     const tags = await screen.findAllByTestId('tag');
